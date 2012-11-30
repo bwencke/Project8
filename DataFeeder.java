268fd21a76e258e5e54de70992b696428ee17265
Ben Wencke <bwencke@purdue.edu>
2012-11-29 22:02:04 -0500

    Added DataFeeder Interface

---
+++ DataFeeder.java
@@ -0,0 +1,13 @@
+public interface DataFeeder {
+
+	public Message getFirstRequest();
+	public Message getFirstResponse();
+	public Message getLastRequest();
+	public Message getLastResponse();
+	public boolean hasNextRequest();
+	public boolean hasNextResponse();
+	public void removeFirstRequest();
+	public void removeFirstResponse();
+	public void removeLastRequest();
+	public void removeLastResponse();
+}
